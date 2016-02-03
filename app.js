07129cd4ab32cd29066c847a02b85955374e61e2
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-02-03 18:32:08 -0500

    Added CORS support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var io = require('socket.io')();
+var cors = require('cors');
 
 var app = express();
 
@@ -14,6 +15,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
